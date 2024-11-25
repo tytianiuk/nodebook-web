ad932f2a18ab694f25089efed3b75db70d253f66
Tytianiuk Artem <tytianiuk@gmail.com>
2024-11-25 15:50:39 +0200

    feat: setup next.js project - setup next.js - add jest and plawright - add prettier and eslint

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/tests/**/*.test.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default createJestConfig(config)
