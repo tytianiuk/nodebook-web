3f50d70ff9747a874fd2ffd6e4aa01dc1f51fad7
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-13 11:25:49 +0200

    feat: change styles of title contacts page

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -4,7 +4,9 @@ const ContactsPage = () => {
   return (
     <div className='bg-white text-black flex justify-center'>
       <div className='px-4 py-12 w-1/2'>
-        <h2 className='mb-8 text-2xl font-semibold'>{"Зв'яжіться з нами"}</h2>
+        <h2 className='mb-8 text-2xl font-semibold text-center md:text-left'>
+          {"Зв'яжіться з нами"}
+        </h2>
         <ContactsForm />
       </div>
     </div>
