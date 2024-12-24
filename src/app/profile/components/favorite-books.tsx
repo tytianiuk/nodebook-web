ad7676d3286f8c31c5d9b468b4c4d93b4a445784
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-25 01:05:27 +0200

    feat: add message when user does not has liked books

--- src/app/profile/components/favorite-books.tsx
+++ src/app/profile/components/favorite-books.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { HeartCrack } from 'lucide-react'
 import { useState, useEffect } from 'react'
 
 import profileAPI from '@/api/profile-api'
@@ -27,15 +28,27 @@ const FavoriteBooks = () => {
     <AccordionItem value='favorite-books' className='px-2'>
       <AccordionTrigger>Вподобані книжки</AccordionTrigger>
       <AccordionContent>
-        <div className='max-h-[600px]  overflow-y-auto pr-4'>
-          <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
-            {favoriteBooks.map((book, index) => (
-              <div className='h-4/5' key={index}>
-                <BookCard book={book} />
-              </div>
-            ))}
+        {!favoriteBooks || favoriteBooks.length === 0 ? (
+          <div className='flex flex-col items-center justify-center py-10 text-center text-gray-500'>
+            <HeartCrack className='w-12 h-12 mb-4' />
+            <p className='text-lg font-medium'>
+              У вас поки немає вподобаних книг.
+            </p>
+            <p className='text-sm'>
+              Перейдіть до каталогу, щоб додати книги у список вподобань.
+            </p>
           </div>
-        </div>
+        ) : (
+          <div className='max-h-[600px]  overflow-y-auto pr-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+              {favoriteBooks.map((book, index) => (
+                <div className='h-4/5' key={index}>
+                  <BookCard book={book} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </AccordionContent>
     </AccordionItem>
   )
