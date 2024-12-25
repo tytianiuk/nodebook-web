a104f61574febab5952e668505691b03002682fd
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-25 17:30:16 +0200

    style: change appearance of scrollbar on catalog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const DynamicBooksList = dynamic(
   () => import('@/app/catalog/components/books-list'),
   {
     loading: () => (
-      <div className='col-span-3 flex flex-col items-center justify-center h-[600px] text-center  '>
+      <div className='col-span-3 flex flex-col items-center justify-center h-full text-center  '>
         <Loader2 className='col-span-3 self-center w-12 h-12 animate-spin text-gray-300' />
         <span className='text-2xl text-gray-300 font-bold mb-2'>
           Завантаження...
@@ -32,10 +32,12 @@ const Catalog: FC<CatalogProps> = async ({ searchParams }) => {
   return (
     <Suspense>
       <div>
-        <h1 className='text-3xl font-bold mb-6'>Каталог книг</h1>
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-6 h-[600px]'>
+        <h1 className='text-3xl font-bold my-6'>Каталог книг</h1>
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-6 h-[calc(100vh-165px)]'>
           <BooksFilters />
-          <DynamicBooksList filters={searchParams} />
+          <div className='md:col-span-3 md:overflow-y-auto'>
+            <DynamicBooksList filters={searchParams} />
+          </div>
         </div>
       </div>
     </Suspense>
