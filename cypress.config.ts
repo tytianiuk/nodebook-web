e6f81ef45255386cf986ebafb3f2f672e19be91d
Tytianiuk Artem <tytianiuk@gmail.com>
2024-12-27 02:24:03 +0200

    ci: add separete ci for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  projectId: 'fyjp1s',
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'tests/e2e/**/*.test.cy.ts',
     supportFile: false,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: false,
+    experimentalStudio: true,
+    defaultCommandTimeout: 10000,
   },
 })
